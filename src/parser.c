5f5a1bce761ccd8e11b40440aa58b420775650c1
Dmitry Moskovenko <moskovenko.dmitry@yandex.ru>
2019-09-23 02:17:46 +0300

    add map creator & piece placer

--- src/parser.c
+++ src/parser.c
@@ -6,7 +6,7 @@
 /*   By: coclayto <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/11 21:45:43 by coclayto          #+#    #+#             */
-/*   Updated: 2019/09/22 00:29:12 by coclayto         ###   ########.fr       */
+/*   Updated: 2019/09/23 01:24:27 by coclayto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,19 +36,20 @@ t_piece	*shifter(t_piece *tetri)
 t_piece	*addpiece(t_piece *tetri, char *str, int size, char letter)
 {
 	int		i;
-	t_piece	*piece;
+	t_piece	*first;
 
 	i = 0;
-	piece = tetri;
+	first = tetri;
 	while ((i + 21) < size)
 	{
 		i += 21;
 		letter++;
-		piece->next = makelist(str + i, letter);
-		piece = piece->next;
+//		printf("letter: %c\n", tetri->letter);
+		tetri->next = makelist(str + i, letter);
+		tetri = tetri->next;
 	}
-	piece->next = NULL;
-	return (tetri);
+	tetri->next = NULL;
+	return (first);
 }
 
 t_piece	*makelist(char *str, char letter)
@@ -79,15 +80,13 @@ t_piece	*makelist(char *str, char letter)
 	return (shifter(piece));
 }
 
-t_piece	*reader(const int fd)
+t_piece		*reader(const int fd)
 {
 	char	buf[545];
 	int		byte_count;
-	int		i;
 	char	letter;
 	t_piece	*tetri;
 
-	i = 0;
 	letter = 'A';
 	if ((byte_count = read(fd, buf, 546)) < 19 || byte_count > 545)
 		return (NULL);
--- src/solver.c
+++ src/solver.c
@@ -6,46 +6,70 @@
 /*   By: coclayto <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/19 20:33:58 by coclayto          #+#    #+#             */
-/*   Updated: 2019/09/22 00:32:31 by coclayto         ###   ########.fr       */
+/*   Updated: 2019/09/23 02:03:33 by coclayto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fillit.h"
 
+char	**insert_piece(char **map, t_piece *tetri)
+{
+	int		x;
+	int		y;
+
+	x = 0;
+	y = 1;
+	while (x <= 7 && y <= 9)
+	{
+		map[tetri->coord[y]][tetri->coord[x]] = tetri->letter;
+		x += 2;
+		y += 2;
+	}
+	return (map);
+}
+
 char		**create_map(t_piece *tetri)
 {
-	t_piece *piece;
+	t_piece	*piece;
 	char	**map;
 	int		piece_count;
 	int		i;
 	
+	piece = tetri;
 	i = 0;
 	piece_count = 0;
-	piece = tetri;
-	while (piece)
+	while (tetri)
 	{
-/*
-		printf("\npiece#%d\n", piece_count + 1);
+/*		printf("piece#%d\n", piece_count + 1);
 		printf("letter: %c\n", tetri->letter);
-		printf("x: %d\n", tetri->coord[0]);
-		printf("y: %d\n", tetri->coord[1]);
-		printf("x: %d\n", tetri->coord[2]);
-		printf("y: %d\n", tetri->coord[3]);
-		printf("x: %d\n", tetri->coord[4]);
-		printf("y: %d\n", tetri->coord[5]);
-		printf("x: %d\n", tetri->coord[6]);
-		printf("y: %d\n", tetri->coord[7]);
+		printf("x1: %d\t", tetri->coord[0]);
+		printf("y1: %d\n", tetri->coord[1]);
+		printf("x2: %d\t", tetri->coord[2]);
+		printf("y2: %d\n", tetri->coord[3]);
+		printf("x3: %d\t", tetri->coord[4]);
+		printf("y3: %d\n", tetri->coord[5]);
+		printf("x4: %d\t", tetri->coord[6]);
+		printf("y5: %d\n", tetri->coord[7]);
+		printf("\n");
 */
-		piece = piece->next;
+		tetri = tetri->next;
 		piece_count++;
 	}
-	map = (char**)malloc(sizeof(*map) * piece_count);
-	while (i < piece_count)
+//	printf("\n");
+	map = (char**)malloc(sizeof(*map) * 4 + 1);
+	while (i < 4)
 	{
-		map[i] = (char*)malloc(sizeof(map) * piece_count + 1);
-		ft_memset(map[i], '.', piece_count);
-		map[i][piece_count + 1] = '\0';
-		printf("map[%d]\n%s\n", i, map[i]);
+		map[i] = (char*)malloc(sizeof(map) * 4 + 1);
+		ft_memset(map[i], '.', 4);
+		map[i][4 + 1] = '\0';
+		i++;
+	}
+	map[i] = NULL;
+	map = insert_piece(map, piece);
+	i = 0;
+	while(i < 4)
+	{
+		printf("%s\n", map[i]);
 		i++;
 	}
 	return (map);
